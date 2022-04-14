52d18db42cea35d6a1510642192c5593d2bb0042
haruyama8940 <yaruo0406@gmail.com>
2022-04-14 22:50:47 +0900

    add episode_num

--- scripts/cmd_dir_image.py
+++ scripts/cmd_dir_image.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from ast import Num
 from platform import node
 
 import rospy
@@ -15,42 +16,43 @@ class cmd_dir_image_node:
         self.file_right = roslib.packages.get_pkg_dir('cmd_dir_image')+ '/images/right.png'
         self.file_left = roslib.packages.get_pkg_dir('cmd_dir_image')+ '/images/left.png'
         self.file_con = roslib.packages.get_pkg_dir('cmd_dir_image')+ '/images/con.png'
-        self.imgCV_go = cv2.imread(self.file_go)  # flagsは省略（デフォ値＝1)
+        self.imgCV_go = cv2.imread(self.file_go)
         self.imgCV_right = cv2.imread(self.file_right)
         self.imgCV_left = cv2.imread(self.file_left)
         self.imgCV_con = cv2.imread(self.file_con)
         self.cmd_dir_list = []
         self.old_cmd_dir_list =[]
-        self.cmd_dir_sub = rospy.Subscriber("/cmd_data",Int8MultiArray, self.cmd_dir_callback)
-        #self.episode_num_sub = rospy.SubscribeListener("episode_num",Int8, self.num_callback)
+        self.step_num
+        self.cmd_dir_sub = rospy.Subscriber("/cmd_dir",Int8MultiArray, self.cmd_dir_callback)
+        self.episode_num_sub = rospy.Subscriber("/episode_num",Int8, self.num_callback)
+        #self.step_num = 100
+
     def cmd_dir_callback(self,data):
         self.cmd_dir_list = data.data #list
-        
+
+    def num_callback(self,data):
+        self.step_num =str(data.data)
+
+    def cv_img(self,img_name):
+        cv2.putText(img_name, self.step_num,(500,100),cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 5)
+        cv2.imshow("dir", img_name)
+
     def loop(self):
-        
-        # print(self.file_go)
-        # print(self.file_left)
-        # print(self.file_right)
-        # print(self.file_con)
 
         if self.old_cmd_dir_list != self.cmd_dir_list:
     
             if self.cmd_dir_list[0] == 100:
-                #cv2.putText(self.imgCV_con,'sleep',(500,100),cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 5)
-                cv2.imshow("dir", self.imgCV_con)
-                
+                self.cv_img(self.imgCV_con)
+
             if self.cmd_dir_list[1] == 100:
-                #cv2.fputText(self.imgCV_go,'sleep',(500,100),cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 5)
-                cv2.imshow("dir", self.imgCV_go)
+                self.cv_img(self.imgCV_go)
                 
             if self.cmd_dir_list[2] == 100:
-                #cv2.putText(self.imgCV_left,'sleep',(500,100),cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 5)
-                cv2.imshow("dir", self.imgCV_left)
+                self.cv_img(self.imgCV_left)
                 
             if self.cmd_dir_list[3] == 100:
-                #cv2.putText(self.imgCV_right,'sleep',(500,100),cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 5)
-                cv2.imshow("dir", self.imgCV_right)
-                
+                self.cv_img(self.imgCV_right)
+
             cv2.waitKey(1000)
         else:
             pass
